fefa85b9a870ac8270ceaf6261fc852062b14a1e
Laaouatni Anas <laaouatni.anas@outlook.it>
2025-04-14 22:47:55 +0200

    the code is all println and seems right but I can't see the serial printlns

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,9 @@
 #include <WebSocketsClient.h>
 #include <WiFi.h>
 
-const WS_SERVER_URL = "pesentiws-43f6274c0f11.herokuapp.com/";
+const String WS_SERVER_URL = "pesentiws-43f6274c0f11.herokuapp.com";
 
-WebSocketClient wsClient;
+WebSocketsClient wsClient;
 
 void setup() {
   Serial.begin(115200);
@@ -14,10 +14,15 @@ void setup() {
   Serial.println("WiFi connected! IP Address: " + WiFi.localIP().toString());
 
   wsClient.beginSSL(WS_SERVER_URL, 443, "/");
+  Serial.println("wsClient.beginSSL: " + WS_SERVER_URL + ", 443, /");
   wsClient.onEvent([](WStype_t type, uint8_t *payload, size_t length) {
-    const hasData = type == WStype_TEXT && length > 0;
+    Serial.println("wsClient.onEvent");
+    const bool hasData = type == WStype_TEXT && length > 0;
+    Serial.println("hasData: " + String(hasData));
     if (!hasData) { return; }
     const String thisWsReceivedStringData = String((char *) payload).substring(0, length);
+    Serial.println("payload: " + String((char *) payload));
+    Serial.println("thisWsReceivedStringData: " + thisWsReceivedStringData);
     Serial.println(thisWsReceivedStringData);
   });
   // wsClient.setReconnectInterval(5 * 1000);
