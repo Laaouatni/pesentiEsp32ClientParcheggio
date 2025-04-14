9b3955905f6853cdfe4a8faeaf489ac9e85ad123
Laaouatni Anas <laaouatni.anas@outlook.it>
2025-04-14 23:16:40 +0200

    save

--- src/main.cpp
+++ src/main.cpp
@@ -15,14 +15,12 @@ void setup() {
   Serial.println("WiFi connected! IP Address: " + WiFi.localIP().toString());
 
   wsClient.beginSSL(WS_SERVER_URL, 443, "/");
-  Serial.println("wsClient.beginSSL: " + WS_SERVER_URL + ", 443, /");
   wsClient.onEvent([](WStype_t type, uint8_t *payload, size_t length) {
     if (length == 0) return;
     const String thisWsReceivedStringData =
         String((char *)payload).substring(0, length);
     if (thisWsReceivedStringData == "/") return;
-    Serial.println(" '" + thisWsReceivedStringData + "' received from server.");
-    // Serial.println(thisWsReceivedStringData);
+    Serial.println(thisWsReceivedStringData);
   });
 }
 
