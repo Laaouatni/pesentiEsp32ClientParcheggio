cc559cec5d9cc7383a597594a9437926b1d5a664
Laaouatni Anas <laaouatni.anas@outlook.it>
2025-04-14 23:15:22 +0200

    funziona la lettura websocket da server heroku

--- src/main.cpp
+++ src/main.cpp
@@ -10,19 +10,20 @@ void setup() {
   Serial.begin(115200);
   Serial.println("Connecting to WiFi...");
   WiFi.begin("nomeWifi", "passwordWifi");
-  while (WiFi.status() != WL_CONNECTED) {};
+  while (WiFi.status() != WL_CONNECTED) {
+  };
   Serial.println("WiFi connected! IP Address: " + WiFi.localIP().toString());
 
   wsClient.beginSSL(WS_SERVER_URL, 443, "/");
   Serial.println("wsClient.beginSSL: " + WS_SERVER_URL + ", 443, /");
   wsClient.onEvent([](WStype_t type, uint8_t *payload, size_t length) {
-    if(length == 0) { return; }
-    const String thisWsReceivedStringData = String((char *) payload).substring(0, length);
+    if (length == 0) return;
+    const String thisWsReceivedStringData =
+        String((char *)payload).substring(0, length);
+    if (thisWsReceivedStringData == "/") return;
     Serial.println(" '" + thisWsReceivedStringData + "' received from server.");
     // Serial.println(thisWsReceivedStringData);
   });
 }
 
-void loop() {
-  wsClient.loop();
-}
+void loop() { wsClient.loop(); }
