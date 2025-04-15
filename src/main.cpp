a9fcd09b48c1865363c242eee48cdb441d095e8f
Laaouatni Anas <laaouatni.anas@outlook.it>
2025-04-15 23:33:03 +0200

    save

--- src/main.cpp
+++ src/main.cpp
@@ -23,30 +23,30 @@ void setup() {
     if (thisWsReceivedStringData == "/") return;
     Serial.println(thisWsReceivedStringData);
 
-    const String wsKey = thisWsReceivedStringData.substring(0, thisWsReceivedStringData.indexOf(":"));
-    const String wsValue = thisWsReceivedStringData.substring(wsKey.length());
-    Serial.println("------");
-    Serial.println("wsKey: " + wsKey);
-    Serial.println("wsValue: " + wsValue);
+    // const String wsKey = thisWsReceivedStringData.substring(0, thisWsReceivedStringData.indexOf(":"));
+    // const String wsValue = thisWsReceivedStringData.substring(thisWsReceivedStringData.indexOf(":") + 1, thisWsReceivedStringData.length());
+    // Serial.println("------");
+    // Serial.println("wsKey: " + wsKey);
+    // Serial.println("wsValue: " + wsValue);
 
-    std::vector<String> splittedStringValues;
-    int lastSplittedStringIndex = 0;
+    // std::vector<String> splittedStringValues;
+    // int lastSplittedStringIndex = 0;
 
-    for (int forCharIndex = 0; forCharIndex < wsValue.length(); forCharIndex++) {
-      const char thisChar = wsValue[forCharIndex];
-      const bool canGoToNextSplittedString = thisChar == ',';
+    // for (int forCharIndex = 0; forCharIndex < wsValue.length(); forCharIndex++) {
+    //   const char thisChar = wsValue[forCharIndex];
+    //   const bool canGoToNextSplittedString = thisChar == ',';
 
-      if(!canGoToNextSplittedString) {
-        splittedStringValues[splittedStringValues.size() - 1] += thisChar;
-        continue;
-      }
-      splittedStringValues.push_back("");
-    }
+    //   if(!canGoToNextSplittedString) {
+    //     splittedStringValues[splittedStringValues.size() - 1] += thisChar;
+    //     continue;
+    //   }
+    //   splittedStringValues.push_back("");
+    // }
 
-    for(String thisSplittedString : splittedStringValues) {
-      Serial.print(thisSplittedString);
-    };
-    Serial.println("-----");
+    // for(String thisSplittedString : splittedStringValues) {
+    //   Serial.print(thisSplittedString);
+    // };
+    // Serial.println("-----");
   });
 }
 
