57ccc5b5233217d36b539702a9849d5f59700992
Laaouatni Anas <laaouatni.anas@outlook.it>
2025-04-15 23:22:09 +0200

    save

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 #include <WebSocketsClient.h>
 #include <WiFi.h>
+#include <vector>
 
 const String WS_SERVER_URL = "pesentiws-43f6274c0f11.herokuapp.com";
 
@@ -21,6 +22,31 @@ void setup() {
         String((char *)payload).substring(0, length);
     if (thisWsReceivedStringData == "/") return;
     Serial.println(thisWsReceivedStringData);
+
+    const String wsKey = thisWsReceivedStringData.substring(0, thisWsReceivedStringData.indexOf(":"));
+    const String wsValue = thisWsReceivedStringData.substring(wsKey.length());
+    Serial.println("------");
+    Serial.println("wsKey: " + wsKey);
+    Serial.println("wsValue: " + wsValue);
+
+    std::vector<String> splittedStringValues;
+    int lastSplittedStringIndex = 0;
+
+    for (int forCharIndex = 0; forCharIndex < wsValue.length(); forCharIndex++) {
+      const char thisChar = wsValue[forCharIndex];
+      const bool canGoToNextSplittedString = thisChar == ',';
+
+      if(!canGoToNextSplittedString) {
+        splittedStringValues[splittedStringValues.size() - 1] += thisChar;
+        continue;
+      }
+      splittedStringValues.push_back("");
+    }
+
+    for(String thisSplittedString : splittedStringValues) {
+      Serial.print(thisSplittedString);
+    };
+    Serial.println("-----");
   });
 }
 
