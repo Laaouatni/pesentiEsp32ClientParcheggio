bed12e19056ae24f7e246f78d9cc8d4502c288bc
laaouatni anas <laaouatni.anas@outlook.it>
2025-04-16 13:25:53 +0200

    save

--- src/main.cpp
+++ src/main.cpp
@@ -2,27 +2,22 @@
 #include <WebSocketsClient.h>
 #include <WiFi.h>
 #include <vector>
-#include <LPD8806.h>
+#include <Adafruit_NeoPixel.h>
 
 const String WS_SERVER_URL = "pesentiws-43f6274c0f11.herokuapp.com";
 
 WebSocketsClient wsClient;
 
-const int nLEDs = 36;
-const int PIN_CLOCK = 6;
-const int PIN_DATA = 15;
+Adafruit_NeoPixel listaLeds = Adafruit_NeoPixel(60, 15, NEO_GRB);
 
-// LPD8806 strip = LPD8806(nLEDs, PIN_DATA, PIN_CLOCK);
-
-// std::vector<int> ledPositions = {35, 32, 30, 20, 17, 15, 6, 3, 0};
+std::vector<int> ledPositions = {0,4,7,20,23,26,38,42,45};
 
 void setup() {
   Serial.begin(115200);
-  strip.begin();
+  listaLeds.begin();
   Serial.println("Connecting to WiFi...");
   WiFi.begin("nomeWifi", "passwordWifi");
-  while (WiFi.status() != WL_CONNECTED) {
-  };
+  while (WiFi.status() != WL_CONNECTED) {};
   Serial.println("WiFi connected! IP Address: " + WiFi.localIP().toString());
 
   wsClient.beginSSL(WS_SERVER_URL, 443, "/");
@@ -35,7 +30,7 @@ void setup() {
     const String wsKey = thisWsReceivedStringData.substring(0, thisWsReceivedStringData.indexOf(":"));
     const String wsValue = thisWsReceivedStringData.substring(thisWsReceivedStringData.indexOf(":")+1);
     Serial.println("wsKey: " + wsKey);
-    Serial.println("wsValue: " + wsValue);
+    // Serial.println("wsValue: " + wsValue);
 
     const char DELIMITER = ',';
     std::vector<int> dimiliterPositions;
@@ -53,18 +48,20 @@ void setup() {
       const int thisDimiliterPosition = forSplittedIndex == 0 ? 0 : dimiliterPositions[forSplittedIndex]+1;
       const int nextDimiliterPosition = forSplittedIndex == 0 ? dimiliterPositions[forSplittedIndex] : dimiliterPositions[forSplittedIndex+1];
       const String thisSplittedString = wsValue.substring(thisDimiliterPosition, nextDimiliterPosition);
-      Serial.println("thisSplittedString: " + thisSplittedString);
+      // Serial.println("thisSplittedString: " + thisSplittedString);
       splittedStringValues.push_back(thisSplittedString);
     }
 
     for (int ledIndex = 0; ledIndex < dimiliterPositions.size(); ledIndex++) {
       const int thisLedPosition = ledPositions[ledIndex];
       const int thisValue = splittedStringValues[ledIndex].toInt();
-      const int choosedColor = thisValue == 0 ? strip.Color(255, 0, 0) : strip.Color(0, 255, 0);
-      // strip.setPixelColor(thisLedPosition, choosedColor);
+      const int choosedColor = thisValue == 1 ? listaLeds.Color(255, 0, 0) : listaLeds.Color(0, 255, 0);
+      listaLeds.setPixelColor(thisLedPosition, choosedColor);
+
+      Serial.println("ledIndex: " + String(ledIndex) + " thisLedPosition: " + String(thisLedPosition) + " thisValue: " + String(thisValue) + " choosedColor: " + String(choosedColor) + " wsValue: " + wsValue);
     }
 
-    // strip.show();
+    listaLeds.show();
   });
 }
 
