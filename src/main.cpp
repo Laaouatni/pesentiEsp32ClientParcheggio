9942e49b7c08da660fb547cfb65b2ce78cde8453
Laaouatni Anas <laaouatni.anas@outlook.it>
2025-05-06 20:10:01 +0200

    save

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,33 @@
-#include <Arduino.h>
-#include <ESP32Servo.h>
 #include "LaaWifiWsBoiderplate.h"
 
-LaaWifiWs laaWifi;
+#include <Arduino.h>
+#include <ESP32Servo.h>
 
+LaaWifiWs laaWifi = LaaWifiWs("pesentiws-43f6274c0f11.herokuapp.com");
+
+struct Cancello {
+    int pin;
+    Servo motore;
+};
+
+Cancello entrata 
 Servo motoreEntrata;
+
 const int pinMotoreEntrata = 15;
+
 Servo motoreUscita;
+
 const int pinMotoreUscita = 2;
 
 void setup() {
   Serial.begin(115200);
-  laaWifi = LaaWifiWs("pesentiws-43f6274c0f11.herokuapp.com")
+  laaWifi.laaOnReceiveMessage()
 }
 
 void loop() {
   laaWifi.laaLoop();
- }
+}
+
+void wsCallbackReceive(String wsKey, String wsValue) {
+
+};
