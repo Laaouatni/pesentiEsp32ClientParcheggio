08bd1841cf1ba0411e9283a74d325ed2059288a5
Laaouatni Anas <laaouatni.anas@outlook.it>
2025-05-06 18:43:22 +0200

    save

--- lib/LaaWifiWsBoiderplate/src/LaaWifiWsBoiderplate.h
+++ lib/LaaWifiWsBoiderplate/src/LaaWifiWsBoiderplate.h
@@ -1,9 +1,19 @@
+#ifndef LAA_WIFI_WS_BOIDERPLATE_H
+#define LAA_WIFI_WS_BOIDERPLATE_H
+
 #include <Arduino.h>
+#include <WebSocketsClient.h>
+#include <WiFi.h>
 
 class LaaWifiWs {
-  public:
-    LaaWifiWs(String wsServerUrl, String wifiNome = "nomeWifi", String wifiPassword = "passwordWifi");
+ public:
+  LaaWifiWs(String wsServerUrl, String wifiNome = "nomeWifi", String wifiPassword = "passwordWifi");
+  void laaOnReceiveMessage(void (*myCallback)(String wsKey, String wsValue));
 
-  private:
-    void laaConnectToWifi(String wifiNome, String wifiPassword);
+ private:
+  WebSocketsClient wsClient;
+  void laaConnectToWifi(String wifiNome, String wifiPassword);
+  void laaConnectToWs(String wsServerUrl);
 };
+
+#endif
