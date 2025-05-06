8c2326e9869ae2c564e156805c24690c16377ed0
Laaouatni Anas <laaouatni.anas@outlook.it>
2025-05-06 20:26:25 +0200

    save

--- src/main.cpp
+++ src/main.cpp
@@ -6,22 +6,16 @@
 LaaWifiWs laaWifi = LaaWifiWs("pesentiws-43f6274c0f11.herokuapp.com");
 
 struct Cancello {
-    int pin;
+    int   pin;
     Servo motore;
 };
 
-Cancello entrata 
-Servo motoreEntrata;
-
-const int pinMotoreEntrata = 15;
-
-Servo motoreUscita;
-
-const int pinMotoreUscita = 2;
+Cancello cancelloEntrata = {.pin = 15, .motore = Servo()};
+Cancello cancelloUscita  = {.pin = 2, .motore = Servo()};
 
 void setup() {
   Serial.begin(115200);
-  laaWifi.laaOnReceiveMessage()
+  laaWifi.laaOnReceiveMessage(&wsCallbackReceive)
 }
 
 void loop() {
@@ -29,5 +23,22 @@ void loop() {
 }
 
 void wsCallbackReceive(String wsKey, String wsValue) {
-
+  logicCancello(wsKey, wsValue);
 };
+
+void logicCancello(String wsKey, String wsValue) {
+  cancelloUscita.motore.attach(cancelloUscita.pin);
+  
+  if (wsKey == "ingresso") { 
+    cancelloEntrata.motore.attach(cancelloEntrata.pin);
+    cancelloEntrata.motore.write(wsValue == "0" ? 90 : 0);
+    delay(250);
+    cancelloEntrata.motore.detach();
+  }
+  if (wsKey == "uscita") { 
+    cancelloUscita.motore.attach(cancelloUscita.pin);
+    cancelloUscita.motore.write(wsValue == "1" ? 180 : 90);
+    delay(250);
+    cancelloUscita.motore.detach();
+   }
+}
