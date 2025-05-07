18749b850164970fbce5e1e47327680672bb4cb7
laaouatni anas <laaouatni.anas@outlook.it>
2025-05-07 13:05:37 +0200

    save funziona il telecomando

--- src/main.cpp
+++ src/main.cpp
@@ -15,7 +15,7 @@ void setup() {
   laaWifi.laaSetup(String("pesentiws-43f6274c0f11.herokuapp.com"), String("nomeWifi"),
                    String("passwordWifi"));
   laaWifi.laaOnReceiveMessage(&wsCallbackReceive);
-}
+};
 
 void loop() {
   laaWifi.laaLoop();
